fbd256d9654f84d4eb211c40e931795ca21879c5
cw-heer-mehta <heer@carwale.com>
2023-06-08 16:12:58 +0530

    final sign up form

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+let validUsername = false;
+let validEmail = false;
+let validPassword = false;
+let validCPassword = false;
+let buttonEnabled = false;
+
 const usernameInput = document.getElementById("username-input");
 
 $("#username-input").on("change", () => {
     var i = $("#username-input").val();
-    validateUsername(i);  
+    validateUsername(i);
 })
 
 
@@ -15,6 +21,10 @@ $("#email-input").on("change", () => {
 $("#password-input").on("change", () => {
     var i = $("#password-input").val();
     validatePassword(i);
+})
+
+$("#c-password-input").on("change", () => {
+    validateConfirmPassword(true);
 })
 
 
@@ -37,51 +47,127 @@ $("#password-show-hide").click(() => {
 
 
 function validateUsername(val) {
-    if(!val || val.length < 3 || val.length > 25){
+    if (!val || val.length < 3 || val.length > 25) {
         $("#username-error").text("Please Enter a valid username that is 3 to 25 characters long");
+        $("#username-input").addClass("wrong");
+        $("#username-input").removeClass("right");
+        validUsername = false;
+        if (buttonEnabled) {
+            disableButton();
+        }
         return;
     }
-    else{
+    else {
         $("#username-error").text("");
+        $("#username-input").addClass("right");
+        $("#username-input").removeClass("wrong");
+        validUsername = true;
+        enableButton();
     }
 }
 
 function validateEmail(val) {
     const emailFormat = /[0-9a-zA-Z! # $ % & ' * + - / = ? ^ _ \{ | \} \.{0,1}]@[a-z0-9-{0,1}\.{0,1}].[a-z0-9]/g;
 
-    const isValid = val.match(emailFormat) ? val.match(emailFormat).length  == 1 : false;
+    const isValid = val.match(emailFormat) ? val.match(emailFormat).length == 1 : false;
 
-    if(!isValid){
+    if (!isValid) {
         $("#email-error").text("Please Enter a valid email address");
+        $("#email-input").addClass("wrong");
+        $("#email-input").removeClass("right");
+        validEmail = false;
+        if (buttonEnabled) {
+            disableButton();
+        }
         return;
     }
-    else{
+    else {
         $("#email-error").text("");
+        $("#email-input").addClass("right");
+        $("#email-input").removeClass("wrong");
+        validEmail = true;
+        enableButton();
     }
 }
 
-function validatePassword(val){
+function validatePassword(val) {
     const passwordFormat = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
     // console.log(passwordFormat.test(val));
-    if(!passwordFormat.test(val)){
+    if (!passwordFormat.test(val)) {
         $("#password-error").text("The password should contain 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character. Please check if the password is valid");
+        $("#password-input").addClass("wrong");
+        $("#password-input").removeClass("right");
+        validPassword = false;
+        if (buttonEnabled) {
+            disableButton();
+        }
         return;
     }
-    else{
+    else {
         $("#password-error").text("");
-        validateConfirmPassword();
+        validateConfirmPassword(false);
+        $("#password-input").addClass("right");
+        $("#password-input").removeClass("wrong");
+        validPassword = true;
+        enableButton();
     }
 }
 
-function validateConfirmPassword(){
+function validateConfirmPassword(fromInput) {
     const password = $("#password-input").val();
     const confirmPassword = $("#c-password-input").val();
 
-    if(confirmPassword != password){
+    if (fromInput) {
+        validatePassword(password);
+    }
+
+    if (confirmPassword != password) {
         $("#confirm-password-error").text("Confirm Password should match the password entered");
+        $("#c-password-input").addClass("wrong");
+        $("#c-password-input").removeClass("right");
+        validCPassword = false;
+        if (buttonEnabled) {
+            disableButton();
+        }
         return;
     }
-    else{
+    else {
         $("#confirm-password-error").text("");
+        $("#c-password-input").addClass("right");
+        $("#c-password-input").removeClass("wrong");
+        validCPassword = true;
+        enableButton();
     }
+}
+
+
+function enableButton() {
+    if (validUsername &&
+        validEmail &&
+        validPassword &&
+        validCPassword) {
+
+        $("#submit-btn")
+            .text("Submit")
+            .removeClass("disabled")
+            .addClass("active-button")
+            .attr("disabled", "false")
+
+        buttonEnabled = true;
+    }
+    else {
+        return;
+    }
+}
+
+
+function disableButton() {
+    $("#submit-btn")
+        .text("Please enter valid data to submit")
+        .removeClass("active-button")
+        .addClass("disabled")
+        .attr("disabled", "true");
+
+    buttonEnabled = false;
+
 }
